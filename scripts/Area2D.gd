003acb9b3f818434f2b706236333e42e326b4166
canbagels <isabel.ibechs@gmail.com>
2024-07-15 23:37:40 -0400

    making UI

--- scripts/Area2D.gd
+++ scripts/Area2D.gd
@@ -1,13 +1,26 @@
 extends Area2D
 
+@onready var playeranimator = %playeranimator
 
 
 @onready var timer = $Timer
 
+var IsDying = false
 
 signal death
-
+func _ready():
+	IsDying = false
 
 func _on_body_entered(_body):
+	IsDying = true
+	playeranimator.play("dying")
+	await get_tree().create_timer(1).timeout
 	print ("you died lol")
-	
+	timer.start()
+
+
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
+
+
+
---
+++ scripts/UI.gd
@@ -0,0 +1,6 @@
+extends CanvasLayer
+
+
+
+func _on_restart_button_pressed():
+	pass # Replace with function body.
